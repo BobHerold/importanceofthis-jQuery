d1a1e0a878d4e2ea7ebda3c870b6c48d8dec2d9a
Bob Herold <bobherold@yahoo.com>
2020-04-27 18:54:05 +0000

    made more changes everywhere as per lessons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-$('p').click(function()) {
-    $(this).slideToggle('slow');
+$('button').mouseenter(function() {
+    $(this).removeClass('makeRed').addClass('makeBlue');
+});
+
+$('button').mouseLeave(function() {
+    $(this).removeClass('makeBlue').addClass('makeRed');
 });
